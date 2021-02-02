6b35b4b027035eb746a0567fd1245aa3ba6aadc5
a-kotova <a.annykot@gmail.com>
2021-02-02 21:11:21 +0300

    Refactor beforeEach hook and get rid of this

--- cypress/integration/tests/checkout/checkout.js
+++ cypress/integration/tests/checkout/checkout.js
@@ -6,26 +6,28 @@ import ShopPage from '../../../page-objects/shopPage';
 const chance = new Chance();
 
 describe('Checkout', () => {
+  let randomProduct;
+
   beforeEach(() => {
-    cy.wrap(ShopPage.pickRandomProduct(products)).as('randomProduct');
+    randomProduct = ShopPage.pickRandomProduct(products);
   });
 
-  it('TA-20: Cart items count should be equal to number of added products', function () {
+  it('TA-20: Cart items count should be equal to number of added products', () => {
     const quantity = chance.integer({ min: 1, max: 12 });
-    ShopPage.addProductToCart(this.randomProduct, quantity);
+    ShopPage.addProductToCart(randomProduct, quantity);
     CheckoutPage.getCartQuantity().should('eq', quantity);
   });
 
-  it('TA-21: Cart total should be correct', function () {
-    ShopPage.addProductToCart(this.randomProduct);
+  it('TA-21: Cart total should be correct', () => {
+    ShopPage.addProductToCart(randomProduct);
     ShopPage.open('/checkout');
-    CheckoutPage.getTotal().should('eq', this.randomProduct.price);
+    CheckoutPage.getTotal().should('eq', randomProduct.price);
   });
 
-  it('TA-22: User is able to increase product quantity', function () {
-    ShopPage.addProductToCart(this.randomProduct);
+  it('TA-22: User is able to increase product quantity', () => {
+    ShopPage.addProductToCart(randomProduct);
     ShopPage.open('/checkout');
-    CheckoutPage.increaseProductQuantity(this.randomProduct);
+    CheckoutPage.increaseProductQuantity(randomProduct);
     CheckoutPage.getCartQuantity().should('eq', 2);
   });
 });
